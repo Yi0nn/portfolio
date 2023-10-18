b35ab97dad9fcff65b8ed5b8686f00f35421e0d4
Yi0nn <stefhania.noguera@correounivalle.edu.co>
2023-10-18 01:25:30 -0500

    Cambios

--- src/World/Floor.jsx
+++ src/World/Floor.jsx
@@ -1,10 +1,19 @@
+import { useTexture } from "@react-three/drei";
 
 const Floor = () => {
-    
+
+    const PATH = "/assets/materials/grass/textures/"
+    const propsTexture = useTexture({
+            map: PATH + 'aerial_grass_rock_diff_1k.jpg',
+        normalMap: PATH + 'aerial_grass_rock_nor_gl_1k.jpg',
+        roughnessMap: PATH + 'aerial_grass_rock_rough_1k.jpg',
+
+    })
+
     return(
         <mesh position-y={-0.5} rotation-x={-Math.PI / 2} receiveShadow ={true}>
             <planeGeometry attach="geometry" args={[12, 12]} />
-            <meshStandardMaterial attach="material" color="pink" />
+            <meshStandardMaterial { ... propsTexture} roughness={-2} metalness={-2} />
         </mesh>
     )
 }
