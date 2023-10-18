39788312bfc492fc75710390c09e22c14be82d6e
Yi0nn <stefhania.noguera@correounivalle.edu.co>
2023-10-18 01:48:52 -0500

    cambios

--- src/World/Cat.jsx
+++ src/World/Cat.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef } from "react";
-import { useGLTF, useAnimations } from "@react-three/drei"; // Importa useGLTF y useAnimations de la forma correcta
-import { ContactShadows } from "@react-three/drei"; // Importa ContactShadows
+import { useEffect, useRef, useState } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { ContactShadows } from "@react-three/drei"; 
 
 const Cat = () => {
     const catRef = useRef();
     const catModel = useGLTF('/assets/models/cat/scene.gltf');
+
+    const [play, setPlay] = useState(false);
+    const [hitSound] = useState(() => new Audio('/assets/sounds/sonido.wav'))
+
     const { animations } = catModel;
     const { actions } = useAnimations(animations, catRef);
     console.log(catModel);
@@ -22,7 +26,12 @@ const Cat = () => {
                 }
             });
         }
-    }, []);
+        if (play) {
+            hitSound.currentTime = 0
+            hitSound.volume = Math.random()
+            hitSound.play();
+        }
+    }, [play]);
 
     return (
         <mesh ref={catRef} position-x={2} position-y={-0.5} position-z={2} rotation-y={-Math.PI * -0.03} scale={0.5} castShadow={true}>
