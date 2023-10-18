bc50a42632799e6d175b33847c45e997257d48cf
Yi0nn <stefhania.noguera@correounivalle.edu.co>
2023-10-18 01:09:33 -0500

    Cambio

--- src/World/Environments.jsx
+++ src/World/Environments.jsx
@@ -1,9 +1,8 @@
-import { Environment, Sparkles, Stars} from "@react-three/drei";
+import { Environment, Cloud, Stars} from "@react-three/drei";
 
 const Environments = () => {
     return(
         <>
-
             <Stars
             radius={100} 
             depth={50} 
@@ -12,6 +11,15 @@ const Environments = () => {
             saturation={0} 
             />
             
+            <Cloud
+            opacity={0.5}
+            speed={0.4} 
+            width={50} 
+            depth={5} 
+            segments={20} 
+            position-y={20}
+            />
+
         </>
     )
 }
